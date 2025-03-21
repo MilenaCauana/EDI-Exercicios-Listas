6ee3aa883d25426d73f5a0f1c49ae1acad552ca5
Milena Cauana <milenarejani@gmail.com>
2025-03-20 21:40:38 -0300

    Inicializando a lista

--- Listas Encadeadas/Lista Estática Encadeada/Teste_Implementação.c
+++ Listas Encadeadas/Lista Estática Encadeada/Teste_Implementação.c
@@ -13,9 +13,9 @@ typedef struct
 
 typedef struct
 {
+    int disp;
     int prim;
-    int prox;
-    Lista A[MAX]; //Aqui, criamos um vetor do tipo Lista, ou seja, A[0] irá guardar o elemento e o próximo
+    Reg A[MAX]; //Aqui, criamos um vetor do tipo Lista, ou seja, A[0] irá guardar o elemento e o próximo
 
 } Lista;
 
@@ -23,13 +23,29 @@ typedef struct
 void Inicializando (Lista *lista)
 {
     int i = 0;
-
+    lista->disp = 0;
+    lista->prim = -1;
+    for (i = 0; i < MAX - 1; i++)
+    {
+        lista->A[i].prox=i+1;
+    }
+    lista->A[i].prox = -1;
 }
 
 int main ()
 {
+    Lista lista;
 
+    //Primeiro, iremos inicializar a lista
+    Inicializando (&lista);
 
+    //Imprimindo as informações da lista inicializada, para conferir
+    printf("Primeiro: %d\n", lista.prim);
+    printf("Disponivel: %d\n", lista.disp);
+    for (int i = 0; i < MAX; i++)
+    {
+        printf("Elemento %d e proximo dele esta na posicao %d\n", lista.A[i].elem, lista.A[i].prox);
+    }
 
 return 0;
 }
